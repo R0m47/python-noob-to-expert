2dfd9f063420fb556d8a53e4e747f7446139be25
R0m47 <faustoroman133@outlook.com>
2025-07-21 18:24:29 -0500

    Modified Password generator

--- level_5/password_generator.py
+++ level_5/password_generator.py
@@ -62,21 +62,14 @@ print("Welcome to the Password Generator!")
 chars_quantity = int(input("How many letters would you like in your password? "))
 symbols_quantity = int(input("How many symbols would you like? "))
 numbers_quantity = int(input("How many numbers would you like? "))
-password = ""
 
 total_quantity_chars = chars_quantity + symbols_quantity + numbers_quantity
-
-while total_quantity_chars > 0:
-    char_type = random.randint(1, 3)
-    if char_type == 1 and chars_quantity != 0:
-        chars_quantity -= 1
-        password = password + letters[random.randint(0, len(letters) - 1)]
-    elif char_type == 2 and symbols_quantity != 0:
-        symbols_quantity -= 1
-        password = password + symbols[random.randint(0, len(symbols) - 1)]
-    else:
-        numbers_quantity -= 1
-        password = password + numbers[random.randint(0, len(numbers) - 1)]
-    total_quantity_chars -= 1
+password_chars = (
+    random.choices(letters, k=chars_quantity)
+    + random.choices(symbols, k=symbols_quantity)
+    + random.choices(numbers, k=numbers_quantity)
+)
+random.shuffle(password_chars)
+password = "".join(password_chars)
 
 print(f"Your password is: {password}")
