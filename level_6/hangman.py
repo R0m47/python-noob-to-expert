8f621b34c0522884047061005a5cf07f21c994a6
R0m47 <faustoroman133@outlook.com>
2025-07-23 11:46:38 -0500

    Added logic

--- level_6/hangman.py
+++ level_6/hangman.py
@@ -1,12 +1,105 @@
 import random
 
+stages = [
+    r"""
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+""",
+    r"""
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+""",
+    r"""
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""",
+    """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+]
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
-guess = input("Guess a letter: ").lower()
+placeholder = ""
+display = ""
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+for space in range(len(chosen_word)):
+    placeholder += "_"
+print(placeholder)
+correct_letters = []
+
+game_over = False
+lives = 6
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+    display = ""
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print("You lose.")
+
+    if "_" not in display:
+        game_over = True
+        print("You win.")
+
+    print(stages[lives])
