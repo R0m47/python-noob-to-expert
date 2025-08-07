fa3fae8a1ddaa6db809f525e41ec7ce4e03b19e4
R0m47 <faustoroman133@outlook.com>
2025-08-06 21:20:34 -0500

    Final version

--- level_9/calculator.py
+++ level_9/calculator.py
@@ -1,37 +1,61 @@
 from calculator_art import logo
+import os
+
+
+def add(first_number, second_number):
+    return first_number + second_number
+
+
+def substract(first_number, second_number):
+    return first_number - second_number
+
+
+def multiply(first_number, second_number):
+    return first_number * second_number
+
+
+def divide(first_number, second_number):
+    return first_number / second_number
+
+
+operations = {
+    "+": add,
+    "-": substract,
+    "*": multiply,
+    "/": divide,
+}
 
 
 def calculate_result(sign, first_number, second_number):
-    if sign == "+":
-        result = first_number + second_number
-    elif sign == "-":
-        result = first_number - second_number
-    elif sign == "*":
-        result = first_number * second_number
-    elif sign == "/":
-        result = first_number / second_number
+    result = operations[sign](first_number, second_number)
     print(f"{first_number} {sign} {second_number} = {result}")
     return result
 
 
-operations = ["+", "-", "*", "/"]
-print(logo)
-continue_calculation = True
-valid_inputs = False
-first_number = 0
-second_number = 0
-while continue_calculation:
-    if first_number == 0:
-        first_number = float(input("What's the first number?: "))
-    for sign in operations:
-        print(f"{sign}\n")
-    operation = input("Pick an operation: ")
-    second_number = float(input("What's the next number?: "))
-    result = calculate_result(operation, first_number, second_number)
-    option = input(
-        f"Type 'y' to continue calculting with {result}, or any else to start a new calculation: "
-    )
-    if option == "y":
-        first_number = result
-    else:
-        first_number = 0
+def calculator():
+    print(logo)
+    continue_calculation = True
+    first_number = 0
+    second_number = 0
+    while continue_calculation:
+        if first_number == 0:
+            first_number = float(input("What's the first number?: "))
+        for sign in operations:
+            print(f"{sign}\n")
+        operation = input("Pick an operation: ")
+        second_number = float(input("What's the next number?: "))
+        result = calculate_result(operation, first_number, second_number)
+        option = input(
+            f"Type 'y' to continue calculting with {result}, type 'e' to exit the program or any key to start a new calculation: "
+        )
+        if option == "y":
+            first_number = result
+        elif option == "e":
+            break
+        else:
+            first_number = 0
+            os.system("cls")
+            calculator()
+
+
+calculator()
