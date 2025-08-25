8b2860b0eb3a8d734f062197af627fd89ef317db
R0m47 <faustoroman133@outlook.com>
2025-08-25 12:21:45 -0500

    Added first step of the game

--- Apprentice/level_17/snake_game.py
+++ Apprentice/level_17/snake_game.py
@@ -4,4 +4,12 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+snake_parts = []
+for positions in starting_positions:
+    snake_part = Turtle(shape="square")
+    snake_part.color("white")
+    snake_part.goto(positions)
+    snake_parts.append(snake_part)
 screen.exitonclick()
