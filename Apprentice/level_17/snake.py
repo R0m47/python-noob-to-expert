9dc052cc214061121bb633cec97c3d8671ae0103
R0m47 <faustoroman133@outlook.com>
2025-08-25 17:43:05 -0500

    Added Food logic

--- Apprentice/level_17/snake.py
+++ Apprentice/level_17/snake.py
@@ -2,12 +2,14 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+TURNING_ANGLE = [0, 90, 180, 270]
 
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for positions in STARTING_POSITIONS:
@@ -23,3 +25,19 @@ class Snake:
             new_y = self.segments[segment - 1].ycor()
             self.segments[segment].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != TURNING_ANGLE[3]:
+            self.head.setheading(TURNING_ANGLE[1])
+
+    def down(self):
+        if self.head.heading() != TURNING_ANGLE[1]:
+            self.head.setheading(TURNING_ANGLE[3])
+
+    def right(self):
+        if self.head.heading() != TURNING_ANGLE[2]:
+            self.head.setheading(TURNING_ANGLE[0])
+
+    def left(self):
+        if self.head.heading() != TURNING_ANGLE[0]:
+            self.head.setheading(TURNING_ANGLE[2])
--- Apprentice/level_17/snake_game.py
+++ Apprentice/level_17/snake_game.py
@@ -1,4 +1,5 @@
 from snake import Snake
+from food import Food
 from turtle import Screen
 import time
 
@@ -9,10 +10,18 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    if snake.head.distance(food) < 20:
+        food.random_location()
 screen.exitonclick()
