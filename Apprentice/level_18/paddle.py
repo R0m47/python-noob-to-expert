c53b7ebfe3500ae02880e17a86d4fc3143c700a2
R0m47 <faustoroman133@outlook.com>
2025-08-26 14:20:43 -0500

    Added paddle object

---
+++ Apprentice/level_18/paddle.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.segments = []
+        self.create_paddle(position)
+
+    def create_paddle(self, position):
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    def up(self):
+        if self.ycor() >= 200:
+            pass
+        else:
+            self.sety(self.ycor() + 20)
+
+    def down(self):
+        if self.ycor() <= -200:
+            pass
+        else:
+            self.sety(self.ycor() - 20)
