f857fa904b547fcc1f1be05d01fbd684c6069c74
R0m47 <faustoroman133@outlook.com>
2025-08-27 17:21:00 -0500

    Modified snake game logic

--- Apprentice/level_17/scoreboard.py
+++ Apprentice/level_17/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
         self.color("white")
         self.penup()
@@ -17,12 +18,18 @@ class Scoreboard(Turtle):
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.write_title()
 
     def write_title(self):
-        self.write(f"Score:{self.score}", align=ALIGMENT, font=FONT)
+        self.clear()
+        self.write(
+            f"Score:{self.score} High Score:{self.high_score}",
+            align=ALIGMENT,
+            font=FONT,
+        )
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.write_title()
