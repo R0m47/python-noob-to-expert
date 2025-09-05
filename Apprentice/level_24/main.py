cb7bea48c9853accc51bcafb064291873ea13bbb
R0m47 <faustoroman133@outlook.com>
2025-09-05 16:04:33 -0500

    Added first part of the project

---
+++ Apprentice/level_24/main.py
@@ -0,0 +1,132 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+
+def create_password():
+    chars_quantity = random.randint(8, 10)
+    symbols_quantity = random.randint(2, 4)
+    numbers_quantity = random.randint(2, 4)
+    password_chars = (
+        random.choices(letters, k=chars_quantity)
+        + random.choices(symbols, k=symbols_quantity)
+        + random.choices(numbers, k=numbers_quantity)
+    )
+    random.shuffle(password_chars)
+    password = "".join(password_chars)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    if website and email and password:
+        is_ok = messagebox.askokcancel(
+            title=website,
+            message=f"These are the details entered: \nEmail:{email}\nPassword:{password}\n Is it ok to save?",
+        )
+        if is_ok:
+            with open("Apprentice/level_24/data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+    else:
+        messagebox.askokcancel(
+            title=website,
+            message=f"Please don't leave any fields empty!",
+        )
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="Apprentice/level_24/logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "fausto@email.com")
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password", command=create_password)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
